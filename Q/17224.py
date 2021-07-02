d7197c4d99b27f62c279cdc32829db59949c4812
Miewone <dlsrk489@gmail.com>
2021-07-03 01:16:49 +0900

    백준 17224

--- Q/17224.py
+++ Q/17224.py
@@ -1,17 +1,44 @@
 # N 문제의 갯수
 # L 현정이의 역량
 # K 풀 수 있는 문제의 최대 개수
-N,L,K = map(int,input().split())
-tp=0
+N, L, K = map(int, input().split())
+AQ = []
+tp = 0
+cnt = 0
 for i in range(N):
-    sub1,sub2 = map(int,input().split())
-    if sub2 <L:
-        tp+=140
-        if i==K:
-            break
-    elif sub1 <L:
-        tp += 100
-        if i==K:
-            break
+    list = []
+    sub1, sub2 = map(int, input().split())
+    list.append(sub1)
+    list.append(sub2)
+    AQ.append(list)
 
+for i in range(1, -1, -1):
+    for j in range(N):
+        if cnt == K:
+            break
+        if AQ[j][i] <= L:
+            if i == 1:
+                AQ[j][i - 1] = L + 1
+                tp += 140
+                cnt += 1
+            else:
+                tp += 100
+                cnt += 1
 print(tp)
+
+# Answer
+
+easy, hard = 0, 0
+for i in range(N):
+    sub1, sub2 = map(int, input().split())
+    if sub2 <= L:
+        hard += 1
+    elif sub1 <= L:
+        easy += 1
+
+ans = min(hard, K) * 140
+
+if hard < K:
+    ans += min(K - hard, easy) * 100
+
+print(ans)
